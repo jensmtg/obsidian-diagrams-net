d083efb6cc9c8e502e810d14fa6b92309e02dd59
SPiCaRia <wlin4@scu.edu>
2023-04-11 10:08:12 -0700

    Nit: clean up tab size issues

--- src/diagrams-view.tsx
+++ src/diagrams-view.tsx
@@ -5,128 +5,128 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 export default class DiagramsView extends ItemView {
-    filePath: string;
-    fileName: string;
-    svgPath: string;
-    xmlPath: string;
-    diagramExists: boolean;
-    hostView: View;
-    vault: Vault;
-    workspace: Workspace;
-    displayText: string;
+	filePath: string;
+	fileName: string;
+	svgPath: string;
+	xmlPath: string;
+	diagramExists: boolean;
+	hostView: View;
+	vault: Vault;
+	workspace: Workspace;
+	displayText: string;
 
-    getDisplayText(): string {
-        return this.displayText ?? 'Diagram';
-    }
+	getDisplayText(): string {
+		return this.displayText ?? 'Diagram';
+	}
 
-    getViewType(): string {
-        return DIAGRAM_VIEW_TYPE;
-    }
+	getViewType(): string {
+		return DIAGRAM_VIEW_TYPE;
+	}
 
-    constructor(leaf: WorkspaceLeaf, hostView: View,
-        initialFileInfo: { path: string, basename: string, svgPath: string, xmlPath: string, diagramExists: boolean }) {
-        super(leaf);
-        this.filePath = initialFileInfo.path;
-        this.fileName = initialFileInfo.basename;
-        this.svgPath = initialFileInfo.svgPath;
-        this.xmlPath = initialFileInfo.xmlPath;
-        this.diagramExists = initialFileInfo.diagramExists;
-        this.vault = this.app.vault;
-        this.workspace = this.app.workspace;
-        this.hostView = hostView
-    }
-
-
+	constructor(leaf: WorkspaceLeaf, hostView: View,
+		initialFileInfo: { path: string, basename: string, svgPath: string, xmlPath: string, diagramExists: boolean }) {
+		super(leaf);
+		this.filePath = initialFileInfo.path;
+		this.fileName = initialFileInfo.basename;
+		this.svgPath = initialFileInfo.svgPath;
+		this.xmlPath = initialFileInfo.xmlPath;
+		this.diagramExists = initialFileInfo.diagramExists;
+		this.vault = this.app.vault;
+		this.workspace = this.app.workspace;
+		this.hostView = hostView
+	}
 
 
-    async onOpen() {
 
-        const handleExit = async () => {
-            close()
-        }
 
-        const handleSaveAndExit = async (msg: any) => {
-            if (this.diagramExists) {
-                saveData(msg)
-                refreshMarkdownViews()
-                close()
-            }
-            else {
-                const { svgFile } = await saveData(msg)
-                insertDiagram(svgFile)
-                close()
-            }
-        }
+	async onOpen() {
 
-        const close = () => {
-            this.workspace.detachLeavesOfType(DIAGRAM_VIEW_TYPE);
-        }
+		const handleExit = async () => {
+			close()
+		}
 
-        const saveData = async (msg: any) => {
-            const svgData = msg.svgMsg.data
-            const svgBuffer = Buffer.from(svgData.replace('data:image/svg+xml;base64,', ''), 'base64')
+		const handleSaveAndExit = async (msg: any) => {
+			if (this.diagramExists) {
+				saveData(msg)
+				refreshMarkdownViews()
+				close()
+			}
+			else {
+				const { svgFile } = await saveData(msg)
+				insertDiagram(svgFile)
+				close()
+			}
+		}
 
-            let svgFile: TFile;
-            let xmlFile: TFile;
-            if (this.diagramExists) {
-                svgFile = this.vault.getAbstractFileByPath(this.svgPath) as TFile
-                xmlFile = this.vault.getAbstractFileByPath(this.xmlPath) as TFile
-                if (!(svgFile instanceof TFile && xmlFile instanceof TFile)) {
-                    return
-                }
-                this.vault.modifyBinary(svgFile, svgBuffer)
-                this.vault.modify(xmlFile, msg.svgMsg.xml)
-            }
-            else {
-                svgFile = await this.vault.createBinary(this.svgPath, svgBuffer)
-                xmlFile = await this.vault.create(this.xmlPath, msg.svgMsg.xml)
+		const close = () => {
+			this.workspace.detachLeavesOfType(DIAGRAM_VIEW_TYPE);
+		}
 
-            }
+		const saveData = async (msg: any) => {
+			const svgData = msg.svgMsg.data
+			const svgBuffer = Buffer.from(svgData.replace('data:image/svg+xml;base64,', ''), 'base64')
 
-            // Return the TFile objects for later usage.
+			let svgFile: TFile;
+			let xmlFile: TFile;
+			if (this.diagramExists) {
+				svgFile = this.vault.getAbstractFileByPath(this.svgPath) as TFile
+				xmlFile = this.vault.getAbstractFileByPath(this.xmlPath) as TFile
+				if (!(svgFile instanceof TFile && xmlFile instanceof TFile)) {
+					return
+				}
+				this.vault.modifyBinary(svgFile, svgBuffer)
+				this.vault.modify(xmlFile, msg.svgMsg.xml)
+			}
+			else {
+				svgFile = await this.vault.createBinary(this.svgPath, svgBuffer)
+				xmlFile = await this.vault.create(this.xmlPath, msg.svgMsg.xml)
+
+			}
+
+			// Return the TFile objects for later usage.
 			// At this point the only use case is that `insertDiagram` needs the
 			// newly created xml TFile object. But just to make the interface
 			// clean, we return both files here, and regardless of newly created
 			// or not.
-            return {
-                svgFile,
-                xmlFile
-            };
-        }
+			return {
+				svgFile,
+				xmlFile
+			};
+		}
 
-        const refreshMarkdownViews = async () => {
-            // Haven't found a way to refresh the hostView.
-        }
+		const refreshMarkdownViews = async () => {
+			// Haven't found a way to refresh the hostView.
+		}
 
-        const insertDiagram = (svgFile: TFile) => {
+		const insertDiagram = (svgFile: TFile) => {
 			// If active file is null, we are at the root folder.
 			const activeFilePath = this.workspace.getActiveFile()?.path ?? '';
 			// Build link text from the svg file to source markdown file.
-            const linkText = this.app.metadataCache.fileToLinktext(svgFile, activeFilePath);
+			const linkText = this.app.metadataCache.fileToLinktext(svgFile, activeFilePath);
 
-            // @ts-ignore: Type not documented.
-            const cursor = this.hostView.editor.getCursor();
-            // @ts-ignore: Type not documented.
-            this.hostView.editor.replaceRange(`![[${linkText}]]`, cursor);
+			// @ts-ignore: Type not documented.
+			const cursor = this.hostView.editor.getCursor();
+			// @ts-ignore: Type not documented.
+			this.hostView.editor.replaceRange(`![[${linkText}]]`, cursor);
 
-        }
+		}
 
-        const container = this.containerEl.children[1];
+		const container = this.containerEl.children[1];
 
-        ReactDOM.render(
-            <DiagramsApp
-                xmlPath={this.xmlPath}
-                diagramExists={this.diagramExists}
-                vault={this.vault}
-                handleExit={handleExit}
-                handleSaveAndExit={handleSaveAndExit}
-            />,
-            container
-        );
-    }
+		ReactDOM.render(
+			<DiagramsApp
+				xmlPath={this.xmlPath}
+				diagramExists={this.diagramExists}
+				vault={this.vault}
+				handleExit={handleExit}
+				handleSaveAndExit={handleSaveAndExit}
+			/>,
+			container
+		);
+	}
 
-    async onClose() {
-        ReactDOM.unmountComponentAtNode(this.containerEl.children[1]);
-    }
+	async onClose() {
+		ReactDOM.unmountComponentAtNode(this.containerEl.children[1]);
+	}
 
 }
